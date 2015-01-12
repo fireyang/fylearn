4cb5d01218e70454b2b13cc9f052ba72c203f066
fireyang <shenhuan007@gmail.com>
2015-01-12 18:31:24 +0800

    load lua file

---
+++ learnC/main.lua
@@ -0,0 +1,3 @@
+local c = 123
+local b = 456;
+print(c+b)
--- learnC/test_lua.c
+++ learnC/test_lua.c
@@ -2,12 +2,43 @@
 #include <lauxlib.h>
 #include <lualib.h>
 #include <stdio.h>
-int
-main()
+
+//初始化lua
+lua_State*
+initLuaEnv()
 {
     lua_State *L = luaL_newstate();
     luaL_openlibs(L);
-    lua_close(L);
-    printf("hello world\n");
+    return L;
+}
+
+//加载lua主文件
+int
+loadLuaFile(lua_State* luaEnv,const char* fileName)
+{
+    int result = luaL_loadfile(luaEnv,fileName);
+    if(result)
+    {
+        return result;
+    }
+    result = lua_pcall(luaEnv,1,0,1);
+    return result;
+}
+
+int
+main()
+{
+    lua_State *luaEnv = initLuaEnv();
+    if(!luaEnv)
+    {
+        return -1;
+    }
+    if(!loadLuaFile(luaEnv,"./main.lua"))
+    {
+        printf("load lua main file Failed!");
+        return -1;
+    }
+    lua_close(luaEnv);
+    //printf("hello world\n");
     return 0;
 }
